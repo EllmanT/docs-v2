44ca16452c9a869d3b2586c972feed17e6f7845c
EllmanT <tmuranda1@gmail.com>
2025-06-26 15:57:16 +0200

    manage plan bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Fiscal Lens",
-  description: "Scan ,analyse, company documents using latest AI Technologies ",
+  description: "Scan ,analyse, company documents using latest AI Technologies. ",
   icons: {
     icon: "/convex.svg",
   },
